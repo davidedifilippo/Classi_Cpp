a5e4d9a0036670bfdcebe999419285f0286383a8
Ing. Di Filippo <davidedifilippo@gmail.com>
2024-05-29 17:20:04 +0200

    Create punto.h

---
+++ Punto_2D/punto.h
@@ -0,0 +1,15 @@
+class Punto {
+public:
+
+	Punto(float ascissa=0, float ordinata=0);
+	
+	float getX();
+    float getY();
+
+	void setX(float x);
+	void setY(float y);
+
+private:
+	float x;
+	float y;
+};
